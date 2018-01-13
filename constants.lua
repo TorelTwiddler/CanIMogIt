73ef987703ebdaf219c5c043eaa9c4ed9ca38078
AmiYuy <amiyuy@gmail.com>
2018-01-13 15:17:06 -0800

    #25 Initial adding of Quests overlay. (Not functional.)

--- constants.lua
+++ constants.lua
@@ -192,3 +192,4 @@ CanIMogIt.NUM_MAIL_INBOX_ITEMS = 7
 -- Merchant Items = MERCHANT_ITEMS_PER_PAGE
 CanIMogIt.NUM_WARDROBE_COLLECTION_BUTTONS = 12 -- Blizzard functions are locals
 -- Trade Skill = no constants
+CanIMogIt.QUEST_INFO_REWARD_ITEMS = 12 -- Blizzard functions are locals
