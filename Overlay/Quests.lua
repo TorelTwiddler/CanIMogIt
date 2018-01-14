fc66d08a0448b76f7cd32d190792df0dcc3f17c7
Salvatore Lopiparo <toreltwiddler@gmail.com>
2018-01-13 18:37:20 -0800

    #25 Initial adding of Quests overlay. (Not functional.) (reverted from commit 89604643751947a13c2a5b8d2c7f46e72310f649)

--- Overlay/Quests.lua
+++ Overlay/Quests.lua
@@ -6,41 +6,9 @@
 ----------------------------
 
 
-function QuestFrame_CIMIUpdateIcon(self)
-    if not self then return end
-    if not CIMI_CheckOverlayIconEnabled() then
-        self.CIMIIconTexture:SetShown(false)
-        self:SetScript("OnUpdate", nil)
-        return
-    end
-
-    -- http://wowprogramming.com/docs/api/GetQuestItemLink
-
-    local itemLink = self:GetParent():GetParent().itemLink
-    if itemLink == nil then
-        CIMI_SetIcon(self, QuestFrame_CIMIUpdateIcon, nil)
-    else
-        CIMI_SetIcon(self, QuestFrame_CIMIUpdateIcon, CanIMogIt:GetTooltipText(itemLink))
-    end
-end
-
-
 ------------------------
 -- Function hooks     --
 ------------------------
-
-
--- http://wowprogramming.com/docs/api/GetNumQuestLogChoices
--- http://wowprogramming.com/docs/api/GetNumQuestLogRewards
-
-function QuestFrame_CIMIOnClick()
-    for i=1,CanIMogIt.QUEST_INFO_REWARD_ITEMS do
-        local frame = _G["MapQuestInfoRewardsFrameQuestInfoItem"..i]
-        if frame then
-            QuestFrame_CIMIUpdateIcon(frame.CanIMogItOverlay)
-        end
-    end
-end
 
 
 ----------------------------
@@ -48,27 +16,6 @@ end
 ----------------------------
 
 
-local function HookOverlayMapQuest(event)
-    if event ~= "PLAYER_LOGIN" then return end
-
-    -- Add hook for the Map Quest Log frames.
-    for i=1,CanIMogIt.QUEST_INFO_REWARD_ITEMS do
-        local frame = _G["MapQuestInfoRewardsFrameQuestInfoItem"..i]
-        if frame then
-            CIMI_AddToFrame(frame, QuestFrame_CIMIUpdateIcon)
-        end
-    end
-end
-
-CanIMogIt.frame:AddEventFunction(HookOverlayMapQuest)
-
-
 ------------------------
 -- Event functions    --
 ------------------------
-
-local function QuestOverlayEvents(event, ...)
-    QuestFrame_CIMIOnClick()
-end
-
-CanIMogIt.frame:AddOverlayEventFunction(QuestOverlayEvents)
