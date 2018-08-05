212090a8879962f4a70cc6f3f99f75a5e0f6e208
Salvatore Lopiparo <toreltwiddler@gmail.com>
2018-08-05 10:02:26 -0700

    Added comment for Resike's fix

--- database.lua
+++ database.lua
@@ -253,6 +253,7 @@ CanIMogIt.itemsToAdd = {}
 
 local function LateAddItems(event, itemID)
     if event == "GET_ITEM_INFO_RECEIVED" and itemID then
+        -- The 8.0.1 update is causing this event to return a bunch of itemID=0
         if itemID <= 0 then
             return
         end
