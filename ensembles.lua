6cc5b0a37b1f1bbf4de14306026a95b61920d140
Victoria Lopiparo <toreltwiddler@gmail.com>
2024-08-03 15:58:52 -0700

    #351 Improve logic for ensembles

--- ensembles.lua
+++ ensembles.lua
@@ -15,18 +15,22 @@ function CanIMogIt:EnsembleItemsKnown(itemLink)
     if itemID == nil then return 0, 0 end
     local setID = C_Item.GetItemLearnTransmogSet(itemID)
     if setID == nil then return 0, 0 end
-    local setAppearances = C_Transmog.GetAllSetAppearancesByID(setID)
-    local known = 0
-    local total = 0
+    local setSources = C_Transmog.GetAllSetAppearancesByID(setID)
+    local knownAppearancesCount = 0
+    local totalAppearancesCount = 0
+    local knownSourcesCount = 0
     -- knownAppearances keys are the "appearanceIDs|armorSubClass" and the values are true if it's known.
     local knownAppearances = {}
     -- totalAppearances keys are the "appearanceIDs|armorSubClass" and the values are always true.
     local totalAppearances = {}
-    if setAppearances == nil then return 0, 0 end
-    for _, source in ipairs(setAppearances) do
+    if setSources == nil then return 0, 0 end
+    for _, source in ipairs(setSources) do
         -- We have to use our custom function for this, since the WoW one doesn't include
         -- checking if items are from the correct armor type.
         local sourceID = source.itemModifiedAppearanceID
+        if C_TransmogCollection.PlayerHasTransmogItemModifiedAppearance(sourceID) then
+            knownSourcesCount = knownSourcesCount + 1
+        end
         local appearanceID = CanIMogIt:GetAppearanceIDFromSourceID(sourceID)
         local sourceItemLink = CanIMogIt:GetItemLinkFromSourceID(sourceID)
         local playerKnowsTransmog = CanIMogIt:PlayerKnowsTransmogFromItem(sourceItemLink)
@@ -39,28 +43,43 @@ function CanIMogIt:EnsembleItemsKnown(itemLink)
         local key = appearanceID .. "|" .. itemSubClass
         if playerKnowsTransmog and not knownAppearances[key] then
             knownAppearances[key] = true
-            known = known + 1
+            knownAppearancesCount = knownAppearancesCount + 1
         end
         if not totalAppearances[key] then
             totalAppearances[key] = true
-            total = total + 1
+            totalAppearancesCount = totalAppearancesCount + 1
         end
     end
-    return known, total
+    return knownAppearancesCount, totalAppearancesCount, knownSourcesCount, #setSources
 end
 
 
 function CanIMogIt:CalculateEnsembleText(itemLink)
     -- Displays the standard KNOWN or UNKNOWN, then include the ratio of known to total.
-    local known, total = CanIMogIt:EnsembleItemsKnown(itemLink)
-    local ratio = known .. "/" .. total
-    if total == 0 then
-        return CanIMogIt.NOT_TRANSMOGABLE, CanIMogIt.NOT_TRANSMOGABLE
-    elseif known == total then
-        return CanIMogIt.KNOWN .. " " .. ratio, CanIMogIt.KNOWN
-    elseif known > 0 then
-        return CanIMogIt.PARTIAL .. " " .. ratio, CanIMogIt.PARTIAL
-    elseif known == 0 then
-        return CanIMogIt.UNKNOWN .. " " .. ratio, CanIMogIt.UNKNOWN
+    local knownAppearances, totalAppearances, knownSources, totalSources = CanIMogIt:EnsembleItemsKnown(itemLink)
+    local ratio = knownAppearances .. "/" .. totalAppearances
+    if CanIMogItOptions["showVerboseText"] then
+        ratio = ratio .. "\nSources: " .. knownSources .. "/" .. totalSources
+        if totalAppearances == 0 then
+            return CanIMogIt.NOT_TRANSMOGABLE .. " " .. ratio, CanIMogIt.NOT_TRANSMOGABLE
+        elseif knownSources == totalSources then
+            return CanIMogIt.KNOWN .. " " .. ratio, CanIMogIt.KNOWN
+        elseif knownAppearances == totalAppearances and knownSources > 0 then
+            return CanIMogIt.KNOWN_FROM_ANOTHER_ITEM .. " " .. ratio, CanIMogIt.KNOWN_FROM_ANOTHER_ITEM
+        elseif knownAppearances > 0 then
+            return CanIMogIt.PARTIAL .. " " .. ratio, CanIMogIt.PARTIAL
+        elseif knownSources == 0 then
+            return CanIMogIt.UNKNOWN .. " " .. ratio, CanIMogIt.UNKNOWN
+        end
+    else
+        if totalAppearances == 0 then
+            return CanIMogIt.NOT_TRANSMOGABLE, CanIMogIt.NOT_TRANSMOGABLE
+        elseif knownAppearances == totalAppearances then
+            return CanIMogIt.KNOWN .. " " .. ratio, CanIMogIt.KNOWN
+        elseif knownAppearances > 0 then
+            return CanIMogIt.PARTIAL .. " " .. ratio, CanIMogIt.PARTIAL
+        elseif knownAppearances == 0 then
+            return CanIMogIt.UNKNOWN .. " " .. ratio, CanIMogIt.UNKNOWN
+        end
     end
 end
