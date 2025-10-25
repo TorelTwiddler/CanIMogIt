b0e8b79b0fd5b2ac37aeec769b8928aaa4c3a03f
Nico <lostitz0r@gmail.com>
2025-10-25 14:48:56 +0200

    #389 Significantly improve tradeskill window handling (from ~30 fps loss when window is open to practically none) - Only update on the following conditions:   - A tradeskill item or header is clicked   - The tradeskill scroll window is being scrolled   - A TRADE_SKILL_UPDATE event is triggered (item crafted, header clicked,   - A TRADE_SKILL_FILTER_UPDATE event is triggered (search filter) - Manually clean up tradeskill overlay icons when option gets disabled - Cache some frames

--- Overlay/Tradeskill.lua
+++ Overlay/Tradeskill.lua
@@ -1,9 +1,18 @@
 -- Overlay for the crafting windows.
 
 
+-- Cached tradeskill recipe button frames in the main list
+local buttons = {}
+
+-- Cached scroll frame and scroll offset
+local scrollFrame = nil
+local scrollFrameOffset = 0
+
+
 ----------------------------
 -- UpdateIcon functions   --
 ----------------------------
+
 
 local function string_starts(String,Start)
     String = String or ""
@@ -11,15 +20,27 @@ local function string_starts(String,Start)
 end
 
 
-function CIMI_UpdateTradeSkillIcons()
+local function RemoveTradeSkillIcons()
+    for i, button in ipairs(buttons) do
+        local itemLink = GetTradeSkillItemLink(button:GetID())
+        if itemLink ~= nil then
+            local text = button.text:GetText() or ""
+            local icon = CanIMogIt:GetIconText(itemLink)
+            if icon ~= nil and string_starts(text, icon) then
+                button.text:SetText(string.gsub(text, icon, ""))
+            end
+        end
+    end
+end
+
+function CIMI_UpdateTradeSkillIcons(self)
     if not CIMI_CheckOverlayIconEnabled() then
+        if self == "OptionUpdate" then RemoveTradeSkillIcons() end
         return
     end
 
     -- Add icon to all visible trade skill recipes
-    -- (Using constant TRADE_SKILLS_DISPLAYED here - other addons can expand crafting UI which also modifies this value)
-    for i=1, TRADE_SKILLS_DISPLAYED do
-        local button = _G["TradeSkillSkill"..i]
+    for i, button in ipairs(buttons) do
         local itemLink = GetTradeSkillItemLink(button:GetID())
         if itemLink ~= nil then
             local text = button.text:GetText() or ""
@@ -34,16 +55,6 @@ end
 
 
 ------------------------
--- Function hooks     --
-------------------------
-
-
-----------------------------
--- Begin adding to frames --
-----------------------------
-
-
-------------------------
 -- Event functions    --
 ------------------------
 
@@ -51,11 +62,35 @@ end
 local function TradeSkillEvents(event, addonName)
     if event == "TRADE_SKILL_SHOW" or event == "ADDON_LOADED" and addonName == "Blizzard_TradeSkillUI" then
         if _G["TradeSkillFrame"] == nil then return end
-        local tradeSkillFrame = _G["TradeSkillFrame"]
-        tradeSkillFrame:HookScript("OnUpdate", CIMI_UpdateTradeSkillIcons)
+        if _G["TradeSkillListScrollFrame"] == nil then return end
+
+        -- Cache scroll frame
+        if not scrollFrame then scrollFrame = _G["TradeSkillListScrollFrame"] end
+
+        -- Cache buttons
+        if not next(buttons) then 
+            -- Using constant TRADE_SKILLS_DISPLAYED as max value - other addons can expand crafting UI which also modifies this value
+            for i=1, TRADE_SKILLS_DISPLAYED do
+                buttons[i] = _G["TradeSkillSkill"..i]
+            end
+        end
+
+        -- Hook update on button click
+        for i, button in ipairs(buttons) do
+            button:HookScript("OnClick", CIMI_UpdateTradeSkillIcons)
+        end
+        
+        -- Hook update when scroll frame offset has changed (i.e. player has scrolled up or down)
+        scrollFrame:HookScript("OnUpdate", function()
+            if (scrollFrameOffset ~= scrollFrame.offset) then
+                scrollFrameOffset = scrollFrame.offset
+                CIMI_UpdateTradeSkillIcons()
+            end
+        end)
     end
 end
-
 CanIMogIt.frame:AddSmartEvent(TradeSkillEvents, {"TRADE_SKILL_SHOW", "ADDON_LOADED"})
+CanIMogIt.frame:AddSmartEvent(CIMI_UpdateTradeSkillIcons, {"TRADE_SKILL_SHOW", "TRADE_SKILL_UPDATE", "TRADE_SKILL_FILTER_UPDATE"})
 
-CanIMogIt:RegisterMessage("OptionUpdate", TradeSkillEvents)
+
+CanIMogIt:RegisterMessage("OptionUpdate", CIMI_UpdateTradeSkillIcons)
